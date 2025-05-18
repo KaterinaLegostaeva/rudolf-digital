17d35ed9bf518e0f41dd9e844c39f2276e836c25
KaterinaLegostaeva <kate.lego71@gmail.com>
2025-05-18 21:59:10 +0300

    major: classes for state machine

--- bot/states.py
+++ bot/states.py
@@ -1,1 +1,10 @@
 # -*- coding: UTF-8 -*-
+from aiogram.fsm import StatesGroup, State
+
+
+class Registration(StatesGroup):
+    vk_id = State()
+
+
+class Tracking(StatesGroup):
+    set_track_number = State()
